4fa46ee77b99f02b5e4bb44a5d0b48e420d93ec9
Abdoboutalat <boutala11@gmail.com>
2018-02-16 10:50:35 +0000

    resolve confle

--- src/main/java/norsys/fr/domain/User2.java
+++ src/main/java/norsys/fr/domain/User2.java
@@ -214,7 +214,6 @@ public class User2 extends AbstractAuditingEntity implements Serializable {
              "login='" + login + '\'' +
             ", firstName='" + firstName + '\'' +
             
-            ", firstName='" + firstName + '\'' +
            
             "}";
     }
