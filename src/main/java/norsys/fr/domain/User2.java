2dc9ed687cb82f68cf08a494360f856d0169530d
Abdoboutalat <boutala11@gmail.com>
2018-02-16 10:21:22 +0000

    ddd

--- src/main/java/norsys/fr/domain/User2.java
+++ src/main/java/norsys/fr/domain/User2.java
@@ -211,6 +211,7 @@ public class User2 extends AbstractAuditingEntity implements Serializable {
     public String toString() {
         return "User{" +
             "login='" + login + '\'' +
+             "login='" + login + '\'' +
             ", firstName='" + firstName + '\'' +
             ", lastName='" + lastName + '\'' +
           
